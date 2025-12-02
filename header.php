8d8e073721a432968b93e5ad39db5461c03b3e63
ziang <ziangshi34@gmail.com>
2025-12-02 12:03:39 +0000

    Enhance navbar to display account type before logout

--- header.php
+++ header.php
@@ -29,14 +29,23 @@
     <li class="nav-item">
     
 <?php
-  // Displays either login or logout on the right, depending on user's
+    // Displays either login or logout on the right, depending on user's
   // current status (session).
   if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) {
-    echo '<a class="nav-link" href="logout.php">Logout</a>';
+
+    # Display account type if set
+    $roleText = '';
+    if (isset($_SESSION['account_type']) && $_SESSION['account_type'] !== '') {
+      // buyer -> Buyer, seller -> Seller
+      $roleText = htmlspecialchars(ucfirst($_SESSION['account_type'])) . ' | ';
+    }
+
+    echo '<a class="nav-link" href="logout.php">' . $roleText . 'Logout</a>';
   }
   else {
     echo '<button type="button" class="btn nav-link" data-toggle="modal" data-target="#loginModal">Login</button>';
   }
+
 ?>
 
     </li>
